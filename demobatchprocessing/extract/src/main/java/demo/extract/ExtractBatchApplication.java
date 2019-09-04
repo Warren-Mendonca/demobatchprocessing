f1843884fff293e0cab32edf9ad4ae9095495c9f
Warren Mendonca <warren.mendonca@gmail.com>
2019-09-04 18:21:32 -0400

    First commit: basic structure

---
+++ demobatchprocessing/extract/src/main/java/demo/extract/ExtractBatchApplication.java
@@ -0,0 +1,13 @@
+package demo.extract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtractBatchApplication.class, args);
+	}
+
+}
---
+++ demobatchprocessing/ingest/src/main/java/demo/ingest/IngestBatchApplication.java
@@ -0,0 +1,13 @@
+package demo.ingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestBatchApplication.class, args);
+	}
+
+}
